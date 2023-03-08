bb2493d719fedd530e7512f56009ed2da14ffedb
troycaselli <swordandtrowel1689@gmail.com>
2023-03-08 14:15:10 -0800

    added key to fix unique index error

--- src/components/pages/DataVisualizations/Graphs/CitizenshipMapAll.jsx
+++ src/components/pages/DataVisualizations/Graphs/CitizenshipMapAll.jsx
@@ -115,8 +115,12 @@ function CitizenshipMapAll(props) {
       />
       <label htmlFor="regionSelect">Select another region below</label>
       <select name="regionSelect" onChange={handleScopeChange}>
-        {geoScopeArray.map(a => {
-          return <option value={a}>{a.toUpperCase()}</option>;
+        {geoScopeArray.map((a, idx) => {
+          return (
+            <option key={idx} value={a}>
+              {a.toUpperCase()}
+            </option>
+          );
         })}
       </select>
       <p>Table view</p>
